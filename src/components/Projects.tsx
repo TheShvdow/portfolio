116cc33cde938a91d547ed438ab73bf3844e6df9
idrissa wade <derisswade@gmail.com>
2024-12-05 09:45:09 +0000

    maj

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,7 @@ const projects = [
     title: "Gestion des Dettes",
     description: "Solution de gestion de dettes d'une boutique avec possibilité de suivi fréquentielle ",
     tech: ["REST API", "Firebase","POSTGRESQL", "Laravel" ,"WebSocket","Swagger"],
-    image: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&q=80&w=800",
+    image: "https://plus.unsplash.com/premium_photo-1676585567527-d25dbeec5b37?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     href: "https://github.com/TheShvdow/gestion_dette_laravel"
   }
 ];
